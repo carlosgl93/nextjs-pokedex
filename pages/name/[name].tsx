5c4cd2a9e011204e887f630a50a98b1297d57442
Carlos Gumucio Labbé <cgumuciolabbe@gmail.com>
2022-04-02 17:48:04 -0300

    added ISG, incremental site regeneration to pokemons by id and name.

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -67,17 +67,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map((name) => ({
       params: { name },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
 
+  const pokemon = getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
       pokemon: await getPokemonInfo(name),
     },
+    revalidate: 86400,
   };
 };
 
